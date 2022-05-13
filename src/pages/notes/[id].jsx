20e1442466788daae9b9c37b961884b1bf9353d1
Dimitar Chetelev <chetelev@gmail.com>
2022-05-13 16:52:42 +0200

    router query

--- src/pages/notes/[id].jsx
+++ src/pages/notes/[id].jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { useRouter, withRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 const Notes = () => {
-    const router = useRouter()
-    const {id} = router.query
+  const router = useRouter();
+  const { id } = router.query;
 
-    return (
-        <h1>Note {id}</h1>
-    )
-}
+  return <h1>Note {id}</h1>;
+};
 
-export default Notes
+export default Notes;
